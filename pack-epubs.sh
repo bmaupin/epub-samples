f8c17e6fe46fb6a63c393fe7c7b6ff3674204c23
bmaupin <bmaupin@users.noreply.github.com>
2015-04-08 15:17:07 -0400

    Ignore .git folder and make more flexible so we can rename EPUB subfolders

--- pack-epubs.sh
+++ pack-epubs.sh
@@ -5,10 +5,11 @@ find . -name '*~' -exec rm '{}' \;
 
 rm *.epub
 
-for folder in `find . -maxdepth 1 -type d ! -path . -printf '%P\n'`; do
+for folder in `find . -maxdepth 1 -type d ! -name .git ! -path . -printf '%P\n'`; do
     cd $folder
-    # http://ebooks.stackexchange.com/a/258/4537
-    zip -rX ../$folder.epub mimetype META-INF/ OEBPS/
+    # Mimetype file must come first
+    zip -X ../$folder.epub mimetype
+    zip -rX ../$folder.epub * -x mimetype
     cd ..
 done
 
